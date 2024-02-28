fe0142d277792e7b8df6e3c9960f6808d0cf8676
Bastian <badmaxyt@gmail.com>
2024-02-28 17:03:27 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MorpionApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Jouer à quel jeu ? Taper [X] pour le morpion et [P] pour le puissance 4.");
+                LancerJeu(ChoisirJeu());
+                Console.WriteLine("Jouer à un autre jeu ? Taper [R] pour changer de jeu. Taper [Echap] pour quitter.");
+            } while (Console.ReadKey(true).Key == ConsoleKey.R);
+        }
+
+        private static ConsoleKey ChoisirJeu()
+        {
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+            } while (key != ConsoleKey.X && key != ConsoleKey.P);
+
+            return key;
+        }
+
+        private static void LancerJeu(ConsoleKey choix)
+        {
+            switch (choix)
+            {
+                case ConsoleKey.X:
+                    Morpion morpion = new Morpion();
+                    morpion.BoucleJeu();
+                    break;
+                case ConsoleKey.P:
+                    PuissanceQuatre puissanceQuatre = new PuissanceQuatre();
+                    puissanceQuatre.BoucleJeu();
+                    break;
+            }
+        }
+    }
+}
