6889f9d0f0f40a2d1240b6e2a9d73a297cab5228
Bastian <badmaxyt@gmail.com>
2024-03-13 16:09:48 +0100

    test: :white_check_mark: add TennisScoreValidite to test validity of inputs

---
+++ TP2/TennisTest/TennisScoreValidite.cs
@@ -0,0 +1,32 @@
+namespace TennisTest;
+
+public class TennisScoreValidite
+{
+    [Fact]
+    [Trait("Tennis", "ScoreValide")]
+    public void TennisScoreNonValide()
+    {
+        var tennis = new Tennis();
+        var echangeJ1 = -1;
+        var echangeJ2 = -1;
+        var scoreAttendu = "Score non valide";
+        
+        var score = tennis.Score(echangeJ1, echangeJ2);
+        
+        Assert.Equal(scoreAttendu, score);
+    }
+    
+    [Fact]
+    [Trait("Tennis", "ScoreValide")]
+    public void TennisScoreValide()
+    {
+        var tennis = new Tennis();
+        var echangeJ1 = 0;
+        var echangeJ2 = 0;
+        var scoreNonAttendu = "Score non valide";
+        
+        var score = tennis.Score(echangeJ1, echangeJ2);
+        
+        Assert.NotEqual(scoreNonAttendu, score);
+    }
+}
