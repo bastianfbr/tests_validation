9159ea4183da8f38587a3a1cdce082c75abde7a6
Bastian <badmaxyt@gmail.com>
2024-03-13 16:10:18 +0100

    feat: :sparkles: add condition to control inputs

--- TP2/TennisTest/Tennis.cs
+++ TP2/TennisTest/Tennis.cs
@@ -4,6 +4,11 @@ public class Tennis
 {
     public string Score(int echangeJ1, int echangeJ2)
     {
+        if (echangeJ1 < 0 || echangeJ2 < 0)
+        {
+            return "Score non valide";
+        }
+        
         if (echangeJ1 == echangeJ2)
         {
             return "Egalite";
