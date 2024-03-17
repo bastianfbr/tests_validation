5151d60072c5539752f8cb0d8743da7cbf9049a6
Bastian <badmaxyt@gmail.com>
2024-03-17 17:25:46 +0100

    feat: :sparkles: add mensualite method

---
+++ TP2/Immobilier/CreditImmobilier.cs
@@ -0,0 +1,20 @@
+﻿namespace Immobilier;
+
+public class CreditImmobilier
+{
+    private double montant;
+    private double tauxAnnuel;
+    private int dureeMois;
+
+    public CreditImmobilier(double montant, double tauxAnnuel, int dureeMois)
+    {
+        this.montant = montant;
+        this.tauxAnnuel = tauxAnnuel;
+        this.dureeMois = dureeMois;
+    }
+    
+    public double mensualite()
+    {
+        return montant * (tauxAnnuel / 100 / 12) / (1 - Math.Pow(1 + tauxAnnuel / 100 / 12, -dureeMois));
+    }
+}
