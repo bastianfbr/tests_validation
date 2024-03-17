675ded5d93b806d33bb711177b52446ee0523e75
Bastian <badmaxyt@gmail.com>
2024-03-17 17:48:50 +0100

    test: :white_check_mark: add DureeMoisEntre9et25ans failing test

--- TP2/ImmobilierTest/Validation.cs
+++ TP2/ImmobilierTest/Validation.cs
@@ -19,6 +19,19 @@ public class Validation
     
     [Fact]
     [Trait("Categorie", "Validation")]
+    public void DureeMoisEntre9et25ans()
+    {
+        var montant = 200000;
+        var tauxAnnuel = 2;
+        var dureeMois = 301;
+    
+        var exception = Assert.Throws<ArgumentException>(() => new CreditImmobilier(montant, tauxAnnuel, dureeMois));
+    
+        Assert.Contains("La durée du crédit doit être entre 9 et 25 ans", exception.Message);
+    }
+    
+    [Fact]
+    [Trait("Categorie", "Validation")]
     public void TauxAnnuelPositif()
     {
         var montant = 200000;
