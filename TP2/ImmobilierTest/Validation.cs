dc02ddf0e1087c89b577e83b80267e27ddf44b3a
Bastian <badmaxyt@gmail.com>
2024-03-17 17:38:06 +0100

    test: :white_check_mark: add TauxAnnuelPositif failing test

--- TP2/ImmobilierTest/Validation.cs
+++ TP2/ImmobilierTest/Validation.cs
@@ -16,5 +16,18 @@ public class Validation
     
         Assert.Contains("Le montant emprunté doit être positif", exception.Message);
     }
+    
+    [Fact]
+    [Trait("Categorie", "Validation")]
+    public void TauxAnnuelPositif()
+    {
+        var montant = 200000;
+        var tauxAnnuel = -2;
+        var dureeMois = 180;
+    
+        var exception = Assert.Throws<ArgumentException>(() => new CreditImmobilier(montant, tauxAnnuel, dureeMois));
+    
+        Assert.Contains("Le taux annuel doit être positif", exception.Message);
+    }
 
 }
