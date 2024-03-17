60768ed138d13a8a765a19e6de308282ceee1e31
Bastian <badmaxyt@gmail.com>
2024-03-17 17:26:24 +0100

    test: :test_tube: edit values of DonnerCoutTotalCredit test

--- TP2/ImmobilierTest/CalculCredit.cs
+++ TP2/ImmobilierTest/CalculCredit.cs
@@ -22,13 +22,13 @@ public class CalculCredit
     [Trait("Categorie", "Calculs")]
     public void DonnerCoutTotalCredit()
     {
-        var montant = 100000;
-        var tauxAnnuel = 0.03;
-        var dureeMois = 120;
+        var montant = 200000;
+        var tauxAnnuel = 2;
+        var dureeMois = 180;
         var credit = new CreditImmobilier(montant, tauxAnnuel, dureeMois);
         
         var coutTotal = credit.coutTotal();
         
-        Assert.Equal(130000, coutTotal);
+        Assert.Equal(231663.13, Math.Round(coutTotal, 2));
     }
 }
