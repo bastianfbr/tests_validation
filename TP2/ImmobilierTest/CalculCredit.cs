23cde9c2befd6f2a7570000404eba3f8da94f11d
Bastian <badmaxyt@gmail.com>
2024-03-17 17:21:06 +0100

    test: :test_tube: add DonnerMensualite failing test

--- TP2/ImmobilierTest/CalculCredit.cs
+++ TP2/ImmobilierTest/CalculCredit.cs
@@ -1,7 +1,23 @@
+using Immobilier;
+
 namespace ImmobilierTest;
 
 public class CalculCredit
 {
+    [Fact]
+    [Trait("Categorie", "Calculs")]
+    public void DonnerMensualite()
+    {
+        var montant = 200000;
+        var tauxAnnuel = 2;
+        var dureeMois = 180;
+        var credit = new CreditImmobilier(montant, tauxAnnuel, dureeMois);
+        
+        var mensualite = credit.mensualite();
+        
+        Assert.Equal(1287.02, mensualite, 2);
+    }
+    
     [Fact]
     [Trait("Categorie", "Calculs")]
     public void DonnerCoutTotalCredit()
